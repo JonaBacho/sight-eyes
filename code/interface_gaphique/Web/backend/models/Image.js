99807a7191b2f25564ab76142cc759aef5e2a6d9
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2025-01-28 18:52:10 +0100

    delete first ui directory and rename second

--- code/interface_gaphique/Web/backend/models/Image.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ImageSchema = new mongoose.Schema({
-    filename: { type: String, required: true },
-    objectName: { type: String, required: true },
-    dateUploaded: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Image', ImageSchema);
